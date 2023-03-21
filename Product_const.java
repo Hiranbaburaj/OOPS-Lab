a10b18ef484d8abd19b7ca23f9c968251308e5a5
Hiranbaburaj <116332096+Hiranbaburaj@users.noreply.github.com>
2023-03-21 11:11:43 +0530

    Update and rename Product.java to Product_const.java

--- Product_const.java
+++ Product_const.java
@@ -14,26 +14,26 @@ public class product{
     public static void main(String[] args){
     
     product p1 = new product("A1", "P1", 550.0);
-    product p2 = new product("B1", "P2", 650.0);
+    product p2 = new product("B1", "P2", 450.0);
     product p3 = new product("C1", "P3", 600.0);
     
-    if(p1.price>p2.price){
-        if(p1.price > p3.price){
-            System.out.println("Product 1 is most Expensive");
+    if(p1.price<p2.price){
+        if(p1.price < p3.price){
+            System.out.println("Product 1 is least Expensive");
         }
 
         else{
-            System.out.println("Product 3 is most Expensive");
+            System.out.println("Product 3 is least Expensive");
         }
     }
 
     else{
-        if(p2.price > p3.price){
-            System.out.println("Product 2 is most Expensive");
+        if(p2.price < p3.price){
+            System.out.println("Product 2 is least Expensive");
         }
 
         else{
-            System.out.println("Product 3 is most Expensive");
+            System.out.println("Product 3 is least Expensive");
         }
     }
     }
